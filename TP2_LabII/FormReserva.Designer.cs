92e481879e00d7d111f63614d0d28b9c619893ef
Paulo <paulo.celentano29@gmail.com>
2024-01-26 21:41:17 -0300

    se sube Proyecto Lab 2

---
+++ TP2_LabII/FormReserva.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TP2_LabII
+{
+    partial class FormReserva
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.MaskedTextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAnular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(54, 45);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(103, 22);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(204, 32);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 49);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(54, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(712, 164);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnAnular
+            // 
+            this.btnAnular.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnAnular.Location = new System.Drawing.Point(608, 39);
+            this.btnAnular.Name = "btnAnular";
+            this.btnAnular.Size = new System.Drawing.Size(139, 53);
+            this.btnAnular.TabIndex = 4;
+            this.btnAnular.Text = "Anular";
+            this.btnAnular.UseVisualStyleBackColor = true;
+            this.btnAnular.Click += new System.EventHandler(this.btnAnular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Codigo";
+            // 
+            // FormReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 286);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAnular);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtCodigo);
+            this.Name = "FormReserva";
+            this.Text = "Cancelar Reserva";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.MaskedTextBox txtCodigo;
+        private System.Windows.Forms.Button btnAnular;
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ TP2_LabII/FormVer.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace TP2_LabII
+{
+    partial class FormVer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnt = new System.Windows.Forms.Button();
+            this.btnSig = new System.Windows.Forms.Button();
+            this.lbServicios = new System.Windows.Forms.ListBox();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.btnReservar = new System.Windows.Forms.Button();
+            this.calReserva = new System.Windows.Forms.MonthCalendar();
+            this.cbxHabitacion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreApellido = new System.Windows.Forms.TextBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.tbCantidad = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAnt
+            // 
+            this.btnAnt.Location = new System.Drawing.Point(12, 122);
+            this.btnAnt.Name = "btnAnt";
+            this.btnAnt.Size = new System.Drawing.Size(47, 78);
+            this.btnAnt.TabIndex = 1;
+            this.btnAnt.Text = "Anterior";
+            this.btnAnt.UseVisualStyleBackColor = true;
+            this.btnAnt.Click += new System.EventHandler(this.btnAnt_Click);
+            // 
+            // btnSig
+            // 
+            this.btnSig.Location = new System.Drawing.Point(384, 122);
+            this.btnSig.Name = "btnSig";
+            this.btnSig.Size = new System.Drawing.Size(47, 78);
+            this.btnSig.TabIndex = 2;
+            this.btnSig.Text = "Siguiente";
+            this.btnSig.UseVisualStyleBackColor = true;
+            this.btnSig.Click += new System.EventHandler(this.btnSig_Click);
+            // 
+            // lbServicios
+            // 
+            this.lbServicios.FormattingEnabled = true;
+            this.lbServicios.ItemHeight = 16;
+            this.lbServicios.Location = new System.Drawing.Point(12, 320);
+            this.lbServicios.Name = "lbServicios";
+            this.lbServicios.Size = new System.Drawing.Size(419, 244);
+            this.lbServicios.TabIndex = 3;
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.Location = new System.Drawing.Point(12, 18);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(419, 296);
+            this.pbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbImagen.TabIndex = 0;
+            this.pbImagen.TabStop = false;
+            // 
+            // btnReservar
+            // 
+            this.btnReservar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnReservar.Location = new System.Drawing.Point(598, 515);
+            this.btnReservar.Name = "btnReservar";
+            this.btnReservar.Size = new System.Drawing.Size(290, 52);
+            this.btnReservar.TabIndex = 4;
+            this.btnReservar.Text = "Reservar";
+            this.btnReservar.UseVisualStyleBackColor = true;
+            // 
+            // calReserva
+            // 
+            this.calReserva.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.calReserva.FirstDayOfWeek = System.Windows.Forms.Day.Monday;
+            this.calReserva.Location = new System.Drawing.Point(598, 298);
+            this.calReserva.MaxSelectionCount = 90;
+            this.calReserva.Name = "calReserva";
+            this.calReserva.ShowToday = false;
+            this.calReserva.TabIndex = 5;
+            this.calReserva.TabStop = false;
+            // 
+            // cbxHabitacion
+            // 
+            this.cbxHabitacion.FormattingEnabled = true;
+            this.cbxHabitacion.Location = new System.Drawing.Point(96, 29);
+            this.cbxHabitacion.Name = "cbxHabitacion";
+            this.cbxHabitacion.Size = new System.Drawing.Size(158, 24);
+            this.cbxHabitacion.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Habitacion";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbxHabitacion);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(696, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 91);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hotel";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.dtpNacimiento);
+            this.groupBox2.Controls.Add(this.txtDni);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtNombreApellido);
+            this.groupBox2.Controls.Add(this.btnCargar);
+            this.groupBox2.Location = new System.Drawing.Point(456, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(548, 182);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cargar Huespedes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fecha de Nacimiento";
+            // 
+            // dtpNacimiento
+            // 
+            this.dtpNacimiento.Location = new System.Drawing.Point(302, 65);
+            this.dtpNacimiento.Name = "dtpNacimiento";
+            this.dtpNacimiento.Size = new System.Drawing.Size(215, 22);
+            this.dtpNacimiento.TabIndex = 5;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(22, 132);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(212, 22);
+            this.txtDni.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DNI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre y Apellido";
+            // 
+            // txtNombreApellido
+            // 
+            this.txtNombreApellido.Location = new System.Drawing.Point(22, 65);
+            this.txtNombreApellido.Name = "txtNombreApellido";
+            this.txtNombreApellido.Size = new System.Drawing.Size(212, 22);
+            this.txtNombreApellido.TabIndex = 1;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(352, 120);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(98, 46);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // tbCantidad
+            // 
+            this.tbCantidad.Location = new System.Drawing.Point(22, 34);
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(178, 22);
+            this.tbCantidad.TabIndex = 15;
+            this.tbCantidad.Leave += new System.EventHandler(this.tbCantidad_Leave);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbCantidad);
+            this.groupBox3.Location = new System.Drawing.Point(456, 18);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(234, 91);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Cantidad Huespedes";
+            // 
+            // FormVer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1020, 574);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.calReserva);
+            this.Controls.Add(this.btnReservar);
+            this.Controls.Add(this.lbServicios);
+            this.Controls.Add(this.btnSig);
+            this.Controls.Add(this.btnAnt);
+            this.Controls.Add(this.pbImagen);
+            this.Name = "FormVer";
+            this.Load += new System.EventHandler(this.FormVer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.ListBox lbServicios;
+        private System.Windows.Forms.Button btnReservar;
+        public System.Windows.Forms.PictureBox pbImagen;
+        public System.Windows.Forms.Button btnAnt;
+        public System.Windows.Forms.Button btnSig;
+        public System.Windows.Forms.MonthCalendar calReserva;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ComboBox cbxHabitacion;
+        public System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreApellido;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpNacimiento;
+        public System.Windows.Forms.TextBox tbCantidad;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
